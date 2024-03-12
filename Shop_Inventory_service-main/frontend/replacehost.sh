85c7c4491580772d6e6cb5fb5c6dc434b9b479ae
stanyromero <97441925+stanyromero@users.noreply.github.com>
2024-03-12 21:11:06 +0530

    Update replacehost.sh

--- Shop_Inventory_service-main/frontend/replacehost.sh
+++ Shop_Inventory_service-main/frontend/replacehost.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#This file will replace the local host from your code and replace with HOST machine's ip in all the files
+
 sed -i "/usr/share/nginx/html/Employee/Customer&SaleRepot.html" -e 's|localhost|'"$IP_HOST"'|g'
 sed -i "/usr/share/nginx/html/Employee/itemsList.html" -e 's|localhost|'"$IP_HOST"'|g'
 sed -i "/usr/share/nginx/html/Employee/Billing/SaleReport(Billing).html" 's|localhost|'"$IP_HOST"'|g'
